fac5ba2f97f05b47d9203e802efb25ca57d61ca3
Ajay Mujumdar <ajay.mujumdar@oracle.com>
2022-01-23 02:57:09 +0530

    first commit

---
+++ CarPooling/BusinessServiceEngine/src/main/java/org/BusinessServiceEngine/MyTestAjay.java
@@ -0,0 +1,10 @@
+package org.BusinessServiceEngine;
+
+public class MyTestAjay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi I am Ajay!! age - 28, java developer");
+	}
+
+}
